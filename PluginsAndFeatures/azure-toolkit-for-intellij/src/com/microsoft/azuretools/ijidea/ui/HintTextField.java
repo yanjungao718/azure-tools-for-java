0bd0b1e817f8e06dc3c3d6ea3c09b1678779f3ec
Rui Fang <rufan@microsoft.com>
2019-10-29 10:58:08 +0800

    Implement HintTextField with IntelliJ SDK to match IntelliJ theme

--- PluginsAndFeatures/azure-toolkit-for-intellij/src/com/microsoft/azuretools/ijidea/ui/HintTextField.java
+++ PluginsAndFeatures/azure-toolkit-for-intellij/src/com/microsoft/azuretools/ijidea/ui/HintTextField.java
@@ -1,66 +1,11 @@
 package com.microsoft.azuretools.ijidea.ui;
 
-import com.intellij.util.ui.UIUtil;
+import com.intellij.ui.components.JBTextField;
+import org.jetbrains.annotations.NotNull;
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.FocusEvent;
-import java.awt.event.FocusListener;
-
-/**
- * Created by vlashch on 2/13/17.
- */
-public class HintTextField extends JTextField implements FocusListener {
-
-    private final String hint;
-    private boolean showingHint;
-
-    public HintTextField(final String hint) {
-        super(hint);
-        setText(hint);
-        this.hint = hint;
-        setShowingHint(true);
-        super.addFocusListener(this);
-    }
-
-    @Override
-    public void focusGained(FocusEvent e) {
-        if (this.getText().isEmpty()) {
-            setShowingHint(false);
-            super.setText("");
-        }
-    }
-
-    @Override
-    public void focusLost(FocusEvent e) {
-        if (this.getText().isEmpty()) {
-            setShowingHint(true);
-            super.setText(hint);
-        }
-    }
-
-    @Override
-    public String getText() {
-        return showingHint ? "" : super.getText();
-    }
-
-    @Override
-    public void setText(String t) {
-        if (t == null || t.isEmpty()) return;
-        super.setText(t);
-        setShowingHint(false);
-    }
-
-    private void setShowingHint(boolean showingHint) {
-        this.showingHint = showingHint;
-        if (!showingHint) {
-            if (UIUtil.isUnderDarcula()) {
-                super.setForeground(new Color(187, 187, 187));
-            } else {
-                super.setForeground(Color.BLACK);
-            }
-        } else {
-            super.setForeground(Color.GRAY);
-        }
+public class HintTextField extends JBTextField {
+    public HintTextField(@NotNull String hint) {
+        super();
+        getEmptyText().setText(hint);
     }
 }
