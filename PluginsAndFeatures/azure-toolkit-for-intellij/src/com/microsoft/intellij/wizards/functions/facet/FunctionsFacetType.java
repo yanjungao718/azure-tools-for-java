665860413030aad740c01b8511923d52eb41dd96
Andy Xu <andxu@microsoft.com>
2020-02-24 14:50:16 +0800

    create a function project in intellj idea.

---
+++ PluginsAndFeatures/azure-toolkit-for-intellij/src/com/microsoft/intellij/wizards/functions/facet/FunctionsFacetType.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) Microsoft Corporation
+ *
+ * All rights reserved.
+ *
+ * MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
+ * to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+ * the Software.
+ *
+ * THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ * THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+package com.microsoft.intellij.wizards.functions.facet;
+
+import com.intellij.facet.Facet;
+import com.intellij.facet.FacetType;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.module.ModuleType;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class FunctionsFacetType extends FacetType<FunctionsFacet, FunctionsFacetConfiguration> {
+    FunctionsFacetType() {
+        super(FunctionsFacet.FACET_TYPE_ID, "AzureFunctions", "AzureFunctions");
+    }
+
+    @Override
+    public FunctionsFacetConfiguration createDefaultConfiguration() {
+        return null;
+    }
+
+    @Override
+    public FunctionsFacet createFacet(@NotNull Module module, String name,
+        @NotNull FunctionsFacetConfiguration configuration, @Nullable Facet underlyingFacet) {
+        return null;
+    }
+
+    @Override
+    public boolean isSuitableModuleType(ModuleType moduleType) {
+        return false;
+    }
+}
