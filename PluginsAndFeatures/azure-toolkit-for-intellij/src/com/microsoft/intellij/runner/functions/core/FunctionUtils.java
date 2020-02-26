4a7a446069d039985240ed5188a190bb2ff77ece
Andy Xu <andxu@microsoft.com>
2020-02-26 10:42:28 +0800

    move writeFunctionJsonFile to function utils

--- PluginsAndFeatures/azure-toolkit-for-intellij/src/com/microsoft/intellij/runner/functions/core/FunctionUtils.java
+++ PluginsAndFeatures/azure-toolkit-for-intellij/src/com/microsoft/intellij/runner/functions/core/FunctionUtils.java
@@ -65,12 +65,7 @@ import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Locale;
-import java.util.Map;
+import java.util.*;
 
 public class FunctionUtils {
 
@@ -166,7 +161,7 @@ public class FunctionUtils {
             if (StringUtils.isNotBlank(config.getKey())) {
                 final File functionJsonFile = Paths.get(stagingFolder.toString(), config.getKey(), FUNCTION_JSON)
                         .toFile();
-                FunctionJsonWriter.writeFunctionJsonFile(functionJsonFile, config.getValue());
+                writeFunctionJsonFile(functionJsonFile, config.getValue());
             }
         }
 
@@ -213,6 +208,33 @@ public class FunctionUtils {
 
     public static String getFuncPath() throws IOException, InterruptedException {
         return FunctionCliResolver.resolveFunc();
+    }
+
+    private static void writeFunctionJsonFile(File file, FunctionConfiguration config) throws IOException {
+        final Map<String, Object> json = new LinkedHashMap<>();
+        json.put("scriptFile", config.getScriptFile());
+        json.put("entryPoint", config.getEntryPoint());
+        final List<Map<String, Object>> lists = new ArrayList<>();
+        if (config.getBindings() != null) {
+            for (final Binding binding : config.getBindings()) {
+                final Map<String, Object> bindingJson = new LinkedHashMap<>();
+                bindingJson.put("type", binding.getType());
+                bindingJson.put("direction", binding.getDirection());
+                bindingJson.put("name", binding.getName());
+                final Map<String, Object> attributes = binding.getBindingAttributes();
+                for (final Map.Entry<String, Object> entry : attributes.entrySet()) {
+                    // Skip 'name' property since we have serialized before the for-loop
+                    if (bindingJson.containsKey(entry.getKey())) {
+                        continue;
+                    }
+                    bindingJson.put(entry.getKey(), entry.getValue());
+                }
+                lists.add(bindingJson);
+            }
+            json.put("bindings", lists.toArray());
+        }
+        file.getParentFile().mkdirs();
+        JsonUtils.writeJsonToFile(file, json);
     }
 
     private static String stripExtraCharacters(String fileName) {
