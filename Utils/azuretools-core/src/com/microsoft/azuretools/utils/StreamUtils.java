140d2e37c4af96745e8bc2d8b7c63c462d211aa5
qianjinshen <qianjinshen@microsoft.com>
2020-09-25 20:57:48 +0800

    add copyright to StreamUtils class.

--- Utils/azuretools-core/src/com/microsoft/azuretools/utils/StreamUtils.java
+++ Utils/azuretools-core/src/com/microsoft/azuretools/utils/StreamUtils.java
@@ -1,3 +1,25 @@
+/*
+ * Copyright (c) Microsoft Corporation
+ *
+ * All rights reserved.
+ *
+ * MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
+ * to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+ * the Software.
+ *
+ * THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ * THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package com.microsoft.azuretools.utils;
 
 import org.apache.commons.lang3.StringUtils;
@@ -13,10 +35,14 @@ public class StreamUtils {
         if (outputStream == null) {
             return StringUtils.EMPTY;
         }
-        return new String(outputStream.toString().getBytes(), charset.name());
+        if (charset != null) {
+            return new String(outputStream.toString().getBytes(), charset.name());
+        } else {
+            return new String(outputStream.toString().getBytes(), StandardCharsets.UTF_8.name());
+        }
     }
 
     public static String toString(OutputStream outputStream) throws UnsupportedEncodingException {
-       return StreamUtils.toString(outputStream, StandardCharsets.UTF_8);
+        return StreamUtils.toString(outputStream, StandardCharsets.UTF_8);
     }
 }
