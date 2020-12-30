c45ec6d138803bce9706254a65efa77be6cb66b5
qianjinshen <qianjinshen@microsoft.com>
2020-12-30 12:20:01 +0800

    add generic actions icons on Function App

--- Utils/azure-explorer-common/src/com/microsoft/tooling/msservices/serviceexplorer/azure/function/FunctionModule.java
+++ Utils/azure-explorer-common/src/com/microsoft/tooling/msservices/serviceexplorer/azure/function/FunctionModule.java
@@ -39,6 +39,7 @@ public class FunctionModule extends AzureRefreshableNode implements FunctionModu
     private static final String FUNCTION_SERVICE_MODULE_ID = FunctionModule.class.getName();
     private static final String ICON_PATH = "azure-functions-small.png";
     private static final String BASE_MODULE_NAME = "Function App(Preview)";
+    protected static final String MODULE_NAME = "Function App";
     private static final String FUNCTION_MODULE = "FunctionModule";
     private static final String FAILED_TO_DELETE_FUNCTION_APP = "Failed to delete Function App %s";
     private static final String ERROR_DELETING_FUNCTION_APP = "Azure Services Explorer - Error Deleting Function App";
