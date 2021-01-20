78da27fba3457f735e34cf7ca8be4755f6c22cb9
qianjinshen <qianjinshen@microsoft.com>
2021-01-20 16:29:41 +0800

    remove unused properties.

--- Utils/azure-explorer-common/src/com/microsoft/tooling/msservices/serviceexplorer/azure/vmarm/VMNode.java
+++ Utils/azure-explorer-common/src/com/microsoft/tooling/msservices/serviceexplorer/azure/vmarm/VMNode.java
@@ -140,13 +140,9 @@ public class VMNode extends RefreshableNode implements TelemetryProperties {
     private static final String WAIT_ICON_PATH = "VirtualMachineUpdating_16.png";
     private static final String STOP_ICON_PATH = "VirtualMachineStopped_16.png";
     private static final String RUN_ICON_PATH = "VirtualMachineRunning_16.png";
-    private static final String ACTION_DELETE = "Delete";
     public static final String ACTION_DOWNLOAD_RDP_FILE = "Connect Remote Desktop";
     private static final String ACTION_SHUTDOWN = "Shutdown";
-    private static final String ACTION_START = "Start";
     private static final String ACTION_SHUTDOWN_ICON = "Stop.png";
-    private static final String ACTION_START_ICON = "Start.png";
-    private static final String ACTION_DELETE_ICON = "Delete.png";
     public static final int REMOTE_DESKTOP_PORT = 3389;
 
     private VirtualMachine virtualMachine;
