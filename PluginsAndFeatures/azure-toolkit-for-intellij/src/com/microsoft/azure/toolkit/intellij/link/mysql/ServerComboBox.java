6558a2a38938f70d9da726a5452821f1738e894c
qianjinshen <qianjinshen@microsoft.com>
2021-02-24 22:36:49 +0800

    added link dialog related ui codes.

---
+++ PluginsAndFeatures/azure-toolkit-for-intellij/src/com/microsoft/azure/toolkit/intellij/link/mysql/ServerComboBox.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ */
+
+package com.microsoft.azure.toolkit.intellij.link.mysql;
+
+import com.microsoft.azure.management.mysql.v2020_01_01.Server;
+import com.microsoft.azure.management.mysql.v2020_01_01.implementation.MySQLManager;
+import com.microsoft.azure.management.resources.Subscription;
+import com.microsoft.azure.toolkit.intellij.common.AzureComboBox;
+import com.microsoft.azuretools.authmanage.AuthMethodManager;
+import com.microsoft.azuretools.sdkmanage.AzureManager;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class ServerComboBox extends AzureComboBox<Server> {
+
+    private Subscription subscription;
+
+    public ServerComboBox() {
+        super(true);
+    }
+
+    public void setSubscription(Subscription subscription) {
+        if (Objects.equals(subscription, this.subscription)) {
+            return;
+        }
+        this.subscription = subscription;
+        if (subscription == null) {
+            this.clear();
+            return;
+        }
+        this.refreshItems();
+    }
+
+    @Override
+    protected String getItemText(Object item) {
+        if (item instanceof Server) {
+            return ((Server) item).name();
+        }
+        return super.getItemText(item);
+    }
+
+    @Override
+    protected List<? extends Server> loadItems() throws Exception {
+        if (subscription == null) {
+            return new ArrayList<>();
+        }
+        AzureManager manager = AuthMethodManager.getInstance().getAzureManager();
+        if (manager == null) {
+            return new ArrayList<>();
+        }
+        final MySQLManager mySQLManager = manager.getMySQLManager(subscription.subscriptionId());
+        return mySQLManager.servers().list();
+    }
+
+    @Override
+    public boolean isRequired() {
+        return true;
+    }
+
+    @Override
+    protected boolean itemContains(List<Server> items, Server value) {
+        if (CollectionUtils.isEmpty(items) || value == null) {
+            return false;
+        }
+        for (Server server : items) {
+            if (StringUtils.equals(server.fullyQualifiedDomainName(), value.fullyQualifiedDomainName())) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
