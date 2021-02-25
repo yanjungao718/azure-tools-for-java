871c239d7107fb4865a5b532a48301166bcba956
Hanxiao Liu <hanli@microsoft.com>
2021-02-25 21:59:41 +0800

    Migrate web app combo box to app service library

--- PluginsAndFeatures/azure-toolkit-for-intellij/src/com/microsoft/azure/toolkit/intellij/webapp/WebAppComboBox.java
+++ PluginsAndFeatures/azure-toolkit-for-intellij/src/com/microsoft/azure/toolkit/intellij/webapp/WebAppComboBox.java
@@ -7,7 +7,7 @@ package com.microsoft.azure.toolkit.intellij.webapp;
 
 import com.intellij.openapi.project.Project;
 import com.microsoft.azure.toolkit.intellij.appservice.AppServiceComboBox;
-import com.microsoft.azure.toolkit.lib.common.operation.AzureOperation;
+import com.microsoft.azure.toolkit.lib.appservice.service.IWebApp;
 import com.microsoft.azure.toolkit.lib.webapp.WebAppService;
 import com.microsoft.azuretools.azurecommons.helpers.NotNull;
 import com.microsoft.azuretools.core.mvp.model.webapp.AzureWebAppMvpModel;
@@ -29,7 +29,7 @@ public class WebAppComboBox extends AppServiceComboBox<WebAppComboBoxModel> {
         webAppCreationDialog.setDeploymentVisible(false);
         webAppCreationDialog.setOkActionListener(webAppConfig -> {
             final WebAppComboBoxModel newModel =
-                new WebAppComboBoxModel(WebAppService.convertConfig2Settings(webAppConfig));
+                    new WebAppComboBoxModel(WebAppService.convertConfig2Settings(webAppConfig));
             newModel.setNewCreateResource(true);
             WebAppComboBox.this.addItem(newModel);
             WebAppComboBox.this.setSelectedItem(newModel);
@@ -40,14 +40,11 @@ public class WebAppComboBox extends AppServiceComboBox<WebAppComboBoxModel> {
 
     @NotNull
     @Override
-    @AzureOperation(
-        name = "webapp.list.detail|subscription|selected",
-        type = AzureOperation.Type.SERVICE
-    )
-    protected List<WebAppComboBoxModel> loadAppServiceModels() throws Exception {
-        return AzureWebAppMvpModel.getInstance().listJavaWebApps(false).parallelStream()
-            .map(WebAppComboBoxModel::new)
-            .sorted((app1, app2) -> app1.getAppName().compareToIgnoreCase(app2.getAppName()))
-            .collect(Collectors.toList());
+    protected List<WebAppComboBoxModel> loadItems() throws Exception {
+        final List<IWebApp> webApps = AzureWebAppMvpModel.getInstance().listAzureWebApps(false);
+        return webApps.stream()
+                      .sorted((a, b) -> a.name().compareToIgnoreCase(b.name()))
+                      .map(webApp -> new WebAppComboBoxModel(webApp))
+                      .collect(Collectors.toList());
     }
 }
--- PluginsAndFeatures/azure-toolkit-for-intellij/src/com/microsoft/azure/toolkit/intellij/webapp/WebAppComboBoxModel.java
+++ PluginsAndFeatures/azure-toolkit-for-intellij/src/com/microsoft/azure/toolkit/intellij/webapp/WebAppComboBoxModel.java
@@ -8,6 +8,8 @@ package com.microsoft.azure.toolkit.intellij.webapp;
 import com.microsoft.azure.management.appservice.OperatingSystem;
 import com.microsoft.azure.management.appservice.WebApp;
 import com.microsoft.azure.toolkit.intellij.appservice.AppServiceComboBoxModel;
+import com.microsoft.azure.toolkit.lib.appservice.model.Runtime;
+import com.microsoft.azure.toolkit.lib.appservice.service.IWebApp;
 import com.microsoft.azuretools.core.mvp.model.AzureMvpModel;
 import com.microsoft.azuretools.core.mvp.model.ResourceEx;
 import com.microsoft.azuretools.core.mvp.model.webapp.WebAppSettingModel;
@@ -19,6 +21,21 @@ public class WebAppComboBoxModel extends AppServiceComboBoxModel<WebApp> {
 
     private String runtime;
     private WebAppSettingModel webAppSettingModel;
+
+    // todo: migrate Base Model to service library
+    public WebAppComboBoxModel(final IWebApp webApp){
+        this.resourceId = webApp.id();
+        this.appName = webApp.name();
+        this.resourceGroup = webApp.entity().getResourceGroup();
+        this.os = StringUtils.capitalize(webApp.getRuntime().getOperatingSystem().getValue());
+        this.subscriptionId = webApp.entity().getSubscriptionId();
+        this.isNewCreateResource = false;
+        final Runtime runtime = webApp.getRuntime();
+        this.runtime = String.format("%s-%s-%s",
+                                     runtime.getOperatingSystem().getValue(),
+                                     runtime.getWebContainer().getValue(),
+                                     runtime.getJavaVersion().getValue());
+    }
 
     public WebAppComboBoxModel(final ResourceEx<WebApp> resourceEx) {
         super(resourceEx);
