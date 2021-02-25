4c881bfda57db1a1852db10d67f87833d8f2343c
Hanxiao Liu <hanli@microsoft.com>
2021-02-25 21:59:41 +0800

    Migrate web app combo box to app service library

--- PluginsAndFeatures/azure-toolkit-for-intellij/src/com/microsoft/azure/toolkit/intellij/webapp/WebAppComboBox.java
+++ PluginsAndFeatures/azure-toolkit-for-intellij/src/com/microsoft/azure/toolkit/intellij/webapp/WebAppComboBox.java
@@ -6,13 +6,11 @@
 package com.microsoft.azure.toolkit.intellij.webapp;
 
 import com.intellij.openapi.project.Project;
-import com.microsoft.azure.management.appservice.WebApp;
 import com.microsoft.azure.toolkit.intellij.appservice.AppServiceComboBox;
+import com.microsoft.azure.toolkit.lib.appservice.service.IWebApp;
 import com.microsoft.azure.toolkit.lib.webapp.WebAppService;
 import com.microsoft.azuretools.azurecommons.helpers.NotNull;
-import com.microsoft.azuretools.core.mvp.model.ResourceEx;
 import com.microsoft.azuretools.core.mvp.model.webapp.AzureWebAppMvpModel;
-import com.microsoft.azuretools.utils.WebAppUtils;
 import com.microsoft.tooling.msservices.components.DefaultLoader;
 
 import java.util.List;
@@ -43,11 +41,10 @@ public class WebAppComboBox extends AppServiceComboBox<WebAppComboBoxModel> {
     @NotNull
     @Override
     protected List<WebAppComboBoxModel> loadItems() throws Exception {
-        final List<ResourceEx<WebApp>> webApps = AzureWebAppMvpModel.getInstance().listAllWebApps(false);
+        final List<IWebApp> webApps = AzureWebAppMvpModel.getInstance().listAzureWebApps(false);
         return webApps.stream()
-                      .filter(resource -> WebAppUtils.isJavaWebApp(resource.getResource()))
-                      .sorted((a, b) -> a.getResource().name().compareToIgnoreCase(b.getResource().name()))
-                      .map(webAppResourceEx -> new WebAppComboBoxModel(webAppResourceEx))
+                      .sorted((a, b) -> a.name().compareToIgnoreCase(b.name()))
+                      .map(webApp -> new WebAppComboBoxModel(webApp))
                       .collect(Collectors.toList());
     }
 }
