0943e1ec09d0223fd9bf804581731271459834e9
Hanxiao Liu <hanli@microsoft.com>
2021-03-09 17:42:07 +0800

    Using executeOnPooledThread in case show progress bar in IntelliJ start up

--- PluginsAndFeatures/azure-toolkit-for-intellij/azure-sdk-reference-book/src/main/java/com/microsoft/azure/toolkit/intellij/sdkreferencebook/activity/AzurePreloadingActivity.java
+++ PluginsAndFeatures/azure-toolkit-for-intellij/azure-sdk-reference-book/src/main/java/com/microsoft/azure/toolkit/intellij/sdkreferencebook/activity/AzurePreloadingActivity.java
@@ -6,14 +6,10 @@
 package com.microsoft.azure.toolkit.intellij.sdkreferencebook.activity;
 
 import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.application.ModalityState;
 import com.intellij.openapi.application.PreloadingActivity;
 import com.intellij.openapi.progress.ProgressIndicator;
-import com.intellij.openapi.progress.ProgressManager;
-import com.intellij.openapi.progress.Task;
 import com.microsoft.azure.toolkit.intellij.sdkreferencebook.service.AzureSDKArtifactService;
 import org.codehaus.plexus.util.ExceptionUtils;
-import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -25,16 +21,12 @@ public class AzurePreloadingActivity extends PreloadingActivity {
     @Override
     public void preload(@org.jetbrains.annotations.NotNull final ProgressIndicator indicator) {
         // Using progress manager as azure task manager is not initialized
-        final Task.Backgroundable backgroundTask = new Task.Backgroundable(null, "Fetching azure libraries", false) {
-            @Override
-            public void run(@NotNull final ProgressIndicator progressIndicator) {
-                try {
-                    AzureSDKArtifactService.getInstance().reloadAzureSDKArtifacts();
-                } catch (IOException e) {
-                    logger.warn(ExceptionUtils.getStackTrace(e)); // preload exception should not block plugin
-                }
+        ApplicationManager.getApplication().executeOnPooledThread(() -> {
+            try {
+                AzureSDKArtifactService.getInstance().reloadAzureSDKArtifacts();
+            } catch (IOException e) {
+                logger.warn(ExceptionUtils.getStackTrace(e)); // preload exception should not block plugin
             }
-        };
-        ApplicationManager.getApplication().invokeLater(() -> ProgressManager.getInstance().run(backgroundTask), ModalityState.any());
+        });
     }
 }
