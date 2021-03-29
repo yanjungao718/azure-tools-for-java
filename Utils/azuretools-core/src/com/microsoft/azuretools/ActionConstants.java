eec910ea2835029e09f2852672ebdc46db4d34bb
Hanxiao Liu <hanli@microsoft.com>
2021-03-29 15:49:26 +0800

    rename do-link to link

--- Utils/azuretools-core/src/com/microsoft/azuretools/ActionConstants.java
+++ Utils/azuretools-core/src/com/microsoft/azuretools/ActionConstants.java
@@ -102,7 +102,7 @@ public final class ActionConstants {
         String CONNECT_TO_SERVER = OPERATION_PREFIX + "connect-to-server";
         String BIND_INTO = OPERATION_PREFIX + "bind-into";
         String LINK_TO_MODULE = OPERATION_PREFIX + "link-to-module";
-        String DO_SERVICE_LINK = OPERATION_PREFIX + "do-link";
+        String DO_SERVICE_LINK = OPERATION_PREFIX + "link";
         String TEST_CONNECTION = OPERATION_PREFIX + "test-connection";
         String UPDATE_PASSWORD = OPERATION_PREFIX + "update-password";
     }
