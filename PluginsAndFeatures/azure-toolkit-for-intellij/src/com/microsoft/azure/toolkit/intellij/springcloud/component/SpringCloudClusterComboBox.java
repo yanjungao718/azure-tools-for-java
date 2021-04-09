414f0121f7ca52b0af7485c970dc83ffff46f64a
Wang Mingliang <wangmi@microsoft.com>
2021-04-09 17:57:10 +0800

    use `Azure.az` to get AzureSpringCloud service.

--- PluginsAndFeatures/azure-toolkit-for-intellij/src/com/microsoft/azure/toolkit/intellij/springcloud/component/SpringCloudClusterComboBox.java
+++ PluginsAndFeatures/azure-toolkit-for-intellij/src/com/microsoft/azure/toolkit/intellij/springcloud/component/SpringCloudClusterComboBox.java
@@ -7,11 +7,11 @@ package com.microsoft.azure.toolkit.intellij.springcloud.component;
 
 import com.microsoft.azure.management.resources.Subscription;
 import com.microsoft.azure.toolkit.intellij.common.AzureComboBox;
-import com.microsoft.azure.toolkit.intellij.springcloud.SpringCloudUtils;
+import com.microsoft.azure.toolkit.lib.Azure;
 import com.microsoft.azure.toolkit.lib.common.operation.AzureOperation;
 import com.microsoft.azure.toolkit.lib.springcloud.AzureSpringCloud;
 import com.microsoft.azure.toolkit.lib.springcloud.SpringCloudCluster;
-import com.microsoft.azuretools.azurecommons.helpers.NotNull;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Collections;
 import java.util.List;
@@ -51,7 +51,7 @@ public class SpringCloudClusterComboBox extends AzureComboBox<SpringCloudCluster
     protected List<? extends SpringCloudCluster> loadItems() throws Exception {
         if (Objects.nonNull(this.subscription)) {
             final String sid = this.subscription.subscriptionId();
-            final AzureSpringCloud az = AzureSpringCloud.az(SpringCloudUtils.getSpringManager(sid));
+            final AzureSpringCloud az = Azure.az(AzureSpringCloud.class);
             return az.clusters();
         }
         return Collections.emptyList();
