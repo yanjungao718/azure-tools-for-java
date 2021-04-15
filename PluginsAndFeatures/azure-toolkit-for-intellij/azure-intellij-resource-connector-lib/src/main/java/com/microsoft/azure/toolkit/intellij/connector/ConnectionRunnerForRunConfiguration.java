c22dd6ee3c39b9f1b524cd2d681e13f8af170f2d
Mingliang Wang <wangmi@microsoft.com>
2021-04-15 16:23:04 +0800

    before run task is not loaded after restart.

--- PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-resource-connector-lib/src/main/java/com/microsoft/azure/toolkit/intellij/connector/ConnectionRunnerForRunConfiguration.java
+++ PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-resource-connector-lib/src/main/java/com/microsoft/azure/toolkit/intellij/connector/ConnectionRunnerForRunConfiguration.java
@@ -5,7 +5,9 @@
 
 package com.microsoft.azure.toolkit.intellij.connector;
 
+import com.intellij.execution.BeforeRunTask;
 import com.intellij.execution.BeforeRunTaskProvider;
+import com.intellij.execution.RunConfigurationExtension;
 import com.intellij.execution.configurations.JavaParameters;
 import com.intellij.execution.configurations.RunConfiguration;
 import com.intellij.execution.configurations.RunConfigurationBase;
@@ -16,6 +18,7 @@ import com.intellij.openapi.util.IconLoader;
 import com.intellij.openapi.util.Key;
 import lombok.Getter;
 import lombok.extern.java.Log;
+import org.jdom.Element;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -24,33 +27,33 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Log
-public class ConnectionRunnerForRunConfiguration extends BeforeRunTaskProvider<ConnectionRunnerForRunConfiguration.BeforeRunTask> {
+public class ConnectionRunnerForRunConfiguration extends BeforeRunTaskProvider<ConnectionRunnerForRunConfiguration.MyBeforeRunTask> {
     @Getter
-    public String name = BeforeRunTask.NAME;
+    public String name = MyBeforeRunTask.NAME;
     @Getter
-    public Key<BeforeRunTask> id = BeforeRunTask.ID;
+    public Key<MyBeforeRunTask> id = MyBeforeRunTask.ID;
     @Getter
-    public Icon icon = BeforeRunTask.ICON;
+    public Icon icon = MyBeforeRunTask.ICON;
 
     @Override
-    public @Nullable Icon getTaskIcon(BeforeRunTask task) {
-        return BeforeRunTask.ICON;
+    public @Nullable Icon getTaskIcon(MyBeforeRunTask task) {
+        return MyBeforeRunTask.ICON;
     }
 
     @Override
-    public String getDescription(BeforeRunTask task) {
-        return BeforeRunTask.DESCRIPTION;
+    public String getDescription(MyBeforeRunTask task) {
+        return MyBeforeRunTask.DESCRIPTION;
     }
 
     @Nullable
     @Override
-    public ConnectionRunnerForRunConfiguration.BeforeRunTask createTask(@NotNull RunConfiguration config) {
-        return isApplicableFor(config) ? new BeforeRunTask(getId()) : null;
+    public ConnectionRunnerForRunConfiguration.MyBeforeRunTask createTask(@NotNull RunConfiguration config) {
+        return new MyBeforeRunTask(getId());
     }
 
     @Override
     public boolean executeTask(@NotNull DataContext dataContext, @NotNull RunConfiguration configuration,
-                               @NotNull ExecutionEnvironment executionEnvironment, @NotNull BeforeRunTask beforeRunTask) {
+                               @NotNull ExecutionEnvironment executionEnvironment, @NotNull ConnectionRunnerForRunConfiguration.MyBeforeRunTask beforeRunTask) {
         return beforeRunTask.execute(dataContext, configuration);
     }
 
@@ -58,16 +61,26 @@ public class ConnectionRunnerForRunConfiguration extends BeforeRunTaskProvider<C
         return ConnectionManager.getDefinitions().stream().anyMatch(d -> d.isApplicableFor(config));
     }
 
-    public static class BeforeRunTask extends com.intellij.execution.BeforeRunTask<BeforeRunTask> {
+    public static class MyBeforeRunTask extends BeforeRunTask<MyBeforeRunTask> {
         private static final String NAME = "Connect Azure Resource";
         private static final String DESCRIPTION = "Connect Azure Resource";
         private static final Icon ICON = IconLoader.getIcon("/icons/Common/Azure.svg");// AzureIconLoader.loadIcon(AzureIconSymbol.Common.AZURE);
-        private static final Key<BeforeRunTask> ID = Key.create("ConnectionRunnerForConfigurationId");
+        private static final Key<MyBeforeRunTask> ID = Key.create("ConnectionRunnerForConfigurationId");
         private List<Connection<? extends Resource, ? extends Resource>> connections;
 
-        protected BeforeRunTask(@NotNull Key<BeforeRunTask> providerId) {
+        protected MyBeforeRunTask(@NotNull Key<MyBeforeRunTask> providerId) {
             super(providerId);
             setEnabled(true);
+        }
+
+        @Override
+        public void writeExternal(@NotNull Element element) {
+            super.writeExternal(element);
+        }
+
+        @Override
+        public void readExternal(@NotNull Element element) {
+            super.readExternal(element);
         }
 
         public boolean execute(@NotNull DataContext dataContext, @NotNull RunConfiguration configuration) {
@@ -78,12 +91,12 @@ public class ConnectionRunnerForRunConfiguration extends BeforeRunTaskProvider<C
         }
     }
 
-    public static class RunConfigurationExtension extends com.intellij.execution.RunConfigurationExtension {
+    public static class MyRunConfigurationExtension extends RunConfigurationExtension {
 
         @Override
         public <T extends RunConfigurationBase> void updateJavaParameters(@NotNull T configuration, @NotNull JavaParameters params, RunnerSettings settings) {
             final @NotNull List<?> beforeTasks = configuration.getBeforeRunTasks();
-            beforeTasks.stream().filter(t -> t instanceof BeforeRunTask).map(t -> (BeforeRunTask) t)
+            beforeTasks.stream().filter(t -> t instanceof MyBeforeRunTask).map(t -> (MyBeforeRunTask) t)
                     .flatMap(t -> t.connections.stream())
                     .forEach(c -> c.updateJavaParametersAtRun(configuration, params));
         }
