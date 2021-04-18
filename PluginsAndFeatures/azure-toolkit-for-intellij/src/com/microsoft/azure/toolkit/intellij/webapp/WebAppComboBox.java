49cdb62214acc421f9cfdf2c7141f04bd7c3b981
Hanxiao Liu <hanli@microsoft.com>
2021-04-18 20:01:54 +0800

    Fix webapp combobox filter condition

--- PluginsAndFeatures/azure-toolkit-for-intellij/src/com/microsoft/azure/toolkit/intellij/webapp/WebAppComboBox.java
+++ PluginsAndFeatures/azure-toolkit-for-intellij/src/com/microsoft/azure/toolkit/intellij/webapp/WebAppComboBox.java
@@ -7,6 +7,7 @@ package com.microsoft.azure.toolkit.intellij.webapp;
 
 import com.intellij.openapi.project.Project;
 import com.microsoft.azure.toolkit.intellij.appservice.AppServiceComboBox;
+import com.microsoft.azure.toolkit.lib.appservice.model.JavaVersion;
 import com.microsoft.azure.toolkit.lib.appservice.service.IWebApp;
 import com.microsoft.azure.toolkit.lib.common.operation.AzureOperation;
 import com.microsoft.azure.toolkit.lib.webapp.WebAppService;
@@ -45,9 +46,10 @@ public class WebAppComboBox extends AppServiceComboBox<WebAppComboBoxModel> {
         name = "webapp.list.detail|subscription|selected",
         type = AzureOperation.Type.SERVICE
     )
-    protected List<WebAppComboBoxModel> loadAppServiceModels() throws Exception {
+    protected List<WebAppComboBoxModel> loadAppServiceModels() {
         final List<IWebApp> webApps = AzureWebAppMvpModel.getInstance().listAzureWebApps(false);
         return webApps.stream()
+                      .filter(webApp -> webApp.getRuntime().getJavaVersion() != null && webApp.getRuntime().getJavaVersion() != JavaVersion.OFF)
                       .sorted((a, b) -> a.name().compareToIgnoreCase(b.name()))
                       .map(webApp -> new WebAppComboBoxModel(webApp))
                       .collect(Collectors.toList());
