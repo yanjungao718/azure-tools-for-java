5e036fd71b29ee5d941d5bedc6ec3306f8262694
Hanxiao Liu <hanli@microsoft.com>
2021-04-18 20:01:54 +0800

    Fix webapp combobox filter condition

--- PluginsAndFeatures/azure-toolkit-for-intellij/src/com/microsoft/azure/toolkit/intellij/webapp/WebAppComboBox.java
+++ PluginsAndFeatures/azure-toolkit-for-intellij/src/com/microsoft/azure/toolkit/intellij/webapp/WebAppComboBox.java
@@ -7,7 +7,9 @@ package com.microsoft.azure.toolkit.intellij.webapp;
 
 import com.intellij.openapi.project.Project;
 import com.microsoft.azure.toolkit.intellij.appservice.AppServiceComboBox;
+import com.microsoft.azure.toolkit.lib.appservice.model.JavaVersion;
 import com.microsoft.azure.toolkit.lib.appservice.service.IWebApp;
+import com.microsoft.azure.toolkit.lib.common.operation.AzureOperation;
 import com.microsoft.azure.toolkit.lib.webapp.WebAppService;
 import com.microsoft.azuretools.azurecommons.helpers.NotNull;
 import com.microsoft.azuretools.core.mvp.model.webapp.AzureWebAppMvpModel;
@@ -29,7 +31,7 @@ public class WebAppComboBox extends AppServiceComboBox<WebAppComboBoxModel> {
         webAppCreationDialog.setDeploymentVisible(false);
         webAppCreationDialog.setOkActionListener(webAppConfig -> {
             final WebAppComboBoxModel newModel =
-                    new WebAppComboBoxModel(WebAppService.convertConfig2Settings(webAppConfig));
+                new WebAppComboBoxModel(WebAppService.convertConfig2Settings(webAppConfig));
             newModel.setNewCreateResource(true);
             WebAppComboBox.this.addItem(newModel);
             WebAppComboBox.this.setSelectedItem(newModel);
@@ -40,9 +42,14 @@ public class WebAppComboBox extends AppServiceComboBox<WebAppComboBoxModel> {
 
     @NotNull
     @Override
-    protected List<WebAppComboBoxModel> loadItems() throws Exception {
+    @AzureOperation(
+        name = "webapp.list.detail|subscription|selected",
+        type = AzureOperation.Type.SERVICE
+    )
+    protected List<WebAppComboBoxModel> loadAppServiceModels() {
         final List<IWebApp> webApps = AzureWebAppMvpModel.getInstance().listAzureWebApps(false);
         return webApps.stream()
+                      .filter(webApp -> webApp.getRuntime().getJavaVersion() != null && webApp.getRuntime().getJavaVersion() != JavaVersion.OFF)
                       .sorted((a, b) -> a.name().compareToIgnoreCase(b.name()))
                       .map(webApp -> new WebAppComboBoxModel(webApp))
                       .collect(Collectors.toList());
