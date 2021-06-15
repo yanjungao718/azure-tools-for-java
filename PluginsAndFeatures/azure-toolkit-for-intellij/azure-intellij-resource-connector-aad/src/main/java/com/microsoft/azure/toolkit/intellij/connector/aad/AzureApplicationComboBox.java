92d072147879f862b0bc739aab18db20aa7c193c
jansorg <github@ja-dev.eu>
2021-06-15 10:25:25 +0200

    Use AzureComboBox's own supplier.

--- PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-resource-connector-aad/src/main/java/com/microsoft/azure/toolkit/intellij/connector/aad/AzureApplicationComboBox.java
+++ PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-resource-connector-aad/src/main/java/com/microsoft/azure/toolkit/intellij/connector/aad/AzureApplicationComboBox.java
@@ -7,26 +7,13 @@ package com.microsoft.azure.toolkit.intellij.connector.aad;
 
 import com.microsoft.azure.toolkit.intellij.common.AzureComboBox;
 import com.microsoft.graph.models.Application;
-import org.jetbrains.annotations.NotNull;
-
-import java.util.List;
-import java.util.function.Supplier;
 
 /**
  * Combobox listing Azure AD applications.
  */
 class AzureApplicationComboBox extends AzureComboBox<Application> {
-    private final Supplier<List<Application>> supplier;
-
-    /**
-     * Create a new application combo box.
-     *
-     * @param supplier Supplies the applications to show. This allows to adjust the applications to show,
-     *                 depending on the context.
-     */
-    AzureApplicationComboBox(@NotNull Supplier<List<Application>> supplier) {
+    AzureApplicationComboBox() {
         super(false);
-        this.supplier = supplier;
 
         this.setEditable(false);
     }
@@ -37,11 +24,5 @@ class AzureApplicationComboBox extends AzureComboBox<Application> {
             return ((Application) item).displayName;
         }
         return super.getItemText(item);
-    }
-
-    @NotNull
-    @Override
-    protected List<? extends Application> loadItems() throws Exception {
-        return supplier.get();
     }
 }
