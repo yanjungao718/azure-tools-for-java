3ba8d36724bfd3ace646db3104edbcaa7837cafa
Hanxiao Liu <hanli@microsoft.com>
2021-06-22 21:01:34 +0800

    Update tomcat image for Eclipse

--- PluginsAndFeatures/azure-toolkit-for-eclipse/com.microsoft.azuretools.container/src/main/java/com/microsoft/azuretools/container/Constant.java
+++ PluginsAndFeatures/azure-toolkit-for-eclipse/com.microsoft.azuretools.container/src/main/java/com/microsoft/azuretools/container/Constant.java
@@ -40,8 +40,9 @@ public class Constant {
     public static final String ERROR_CREATING_DOCKERFILE = "Error occurred in generating Dockerfile, "
             + "with exception:\n%s";
     public static final String ERROR_RUNNING_DOCKER = "Error occurred in Docker Run, with exception:\n%s";
-    public static final String DOCKERFILE_CONTENT_TOMCAT = "FROM tomcat:8.5-jre8\r\n"
-            + "RUN rm -fr /usr/local/tomcat/webapps/ROOT\r\n" + "COPY %s /usr/local/tomcat/webapps/ROOT.war\r\n";
+    public static final String DOCKERFILE_CONTENT_TOMCAT = "FROM mcr.microsoft.com/java/tomcat:8-zulu-alpine-tomcat-9"
+            + System.lineSeparator() + "RUN rm -fr /usr/local/tomcat/webapps/ROOT" + System.lineSeparator()
+            + "COPY %s /usr/local/tomcat/webapps/ROOT.war" + System.lineSeparator();
     public static final String DOCKERFILE_CONTENT_SPRING = "FROM azul/zulu-openjdk-alpine:8\r\n" + "VOLUME /tmp\r\n"
             + "EXPOSE 8080\r\n" + "COPY %s app.jar\r\n"
             + "ENTRYPOINT java -Djava.security.egd=file:/dev/./urandom -jar /app.jar";
