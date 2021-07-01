b70cc5fac55c55f33e2e4a14da433a2d70ebfdef
andxu <andxu@microsoft.com>
2021-07-01 17:47:21 +0800

    restore intelli proxy selector

--- PluginsAndFeatures/azure-toolkit-for-intellij/src/com/microsoft/intellij/ProxyUtils.java
+++ PluginsAndFeatures/azure-toolkit-for-intellij/src/com/microsoft/intellij/ProxyUtils.java
@@ -8,7 +8,7 @@ package com.microsoft.intellij;
 import com.azure.core.util.Configuration;
 import com.intellij.util.net.HttpConfigurable;
 import com.microsoft.azure.toolkit.lib.Azure;
-import com.microsoft.azure.toolkit.lib.common.logging.Log;
+import com.microsoft.azure.toolkit.lib.common.messager.AzureMessager;
 import com.microsoft.azure.toolkit.lib.common.proxy.ProxyManager;
 import com.microsoft.azure.toolkit.lib.common.utils.TextUtils;
 
@@ -22,11 +22,10 @@ public class ProxyUtils {
         proxyManager.init();
         String source = "system";
         if (instance != null && instance.USE_HTTP_PROXY) {
-            proxyManager.configure(instance.PROXY_HOST, instance.PROXY_PORT);
             source = "intellij";
         }
         if (source != null && Objects.nonNull(proxyManager.getProxy())) {
-            Log.info(String.format("Use %s proxy: %s:%s", source, TextUtils.cyan(proxyManager.getHttpProxyHost()),
+            AzureMessager.getMessager().info(String.format("Use %s proxy: %s:%s", source, TextUtils.cyan(proxyManager.getHttpProxyHost()),
                 TextUtils.cyan(Integer.toString(proxyManager.getHttpProxyPort()))));
 
             // set proxy for azure-identity according to https://docs.microsoft.com/en-us/azure/developer/java/sdk/proxying
