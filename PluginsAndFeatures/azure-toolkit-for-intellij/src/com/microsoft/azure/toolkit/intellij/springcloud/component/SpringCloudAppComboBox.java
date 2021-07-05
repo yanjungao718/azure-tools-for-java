13f06e9a5db86c0d3307864db502ac8f62169f9e
Mingliang Wang <wangmi@microsoft.com>
2021-07-05 16:31:28 +0800

    prevent adding draft app multiple times when restore a run configuration

--- PluginsAndFeatures/azure-toolkit-for-intellij/src/com/microsoft/azure/toolkit/intellij/springcloud/component/SpringCloudAppComboBox.java
+++ PluginsAndFeatures/azure-toolkit-for-intellij/src/com/microsoft/azure/toolkit/intellij/springcloud/component/SpringCloudAppComboBox.java
@@ -14,18 +14,18 @@ import com.microsoft.azure.toolkit.lib.springcloud.SpringCloudApp;
 import com.microsoft.azure.toolkit.lib.springcloud.SpringCloudCluster;
 import com.microsoft.azuretools.azurecommons.helpers.NotNull;
 import com.microsoft.azuretools.azurecommons.helpers.Nullable;
-import org.apache.commons.collections.CollectionUtils;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
-import java.util.stream.Collectors;
 
 import static com.microsoft.intellij.ui.messages.AzureBundle.message;
 
 public class SpringCloudAppComboBox extends AzureComboBox<SpringCloudApp> {
     private SpringCloudCluster cluster;
-    private final List<SpringCloudApp> localItems = new ArrayList<>();
+    private final Map<String, SpringCloudApp> localItems = new HashMap<>();
 
     @Override
     protected String getItemText(final Object item) {
@@ -61,10 +61,8 @@ public class SpringCloudAppComboBox extends AzureComboBox<SpringCloudApp> {
     protected List<? extends SpringCloudApp> loadItems() throws Exception {
         final List<SpringCloudApp> apps = new ArrayList<>();
         if (Objects.nonNull(this.cluster)) {
-            if (CollectionUtils.isNotEmpty(this.localItems)) {
-                apps.addAll(this.localItems.stream()
-                    .filter(i -> Objects.equals(this.cluster.name(), i.getCluster().name()))
-                    .collect(Collectors.toList()));
+            if (!this.localItems.isEmpty()) {
+                apps.add(this.localItems.get(this.cluster.name()));
             }
             apps.addAll(cluster.apps());
         }
@@ -93,9 +91,12 @@ public class SpringCloudAppComboBox extends AzureComboBox<SpringCloudApp> {
     }
 
     public void addLocalItem(SpringCloudApp app) {
-        this.localItems.add(0, app);
-        final List<SpringCloudApp> items = this.getItems();
-        items.add(0, app);
-        this.setItems(items);
+        final SpringCloudApp cached = this.localItems.get(app.getCluster().name());
+        if (Objects.isNull(cached) || !Objects.equals(app.name(), cached.name())) {
+            this.localItems.put(app.getCluster().name(), app);
+            final List<SpringCloudApp> items = this.getItems();
+            items.add(0, app);
+            this.setItems(items);
+        }
     }
 }
