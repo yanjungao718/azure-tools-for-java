97c6bf44a490c048f78148fb8f1889dbb07c5a0c
shenqianjin <qianjinshen@microsoft.com>
2021-07-27 21:36:59 +0800

    Fix class cast error in sql connect dialog.

--- PluginsAndFeatures/azure-toolkit-for-intellij/src/com/microsoft/azure/toolkit/intellij/connector/database/DatabaseResourcePanel.java
+++ PluginsAndFeatures/azure-toolkit-for-intellij/src/com/microsoft/azure/toolkit/intellij/connector/database/DatabaseResourcePanel.java
@@ -295,7 +295,7 @@ public class DatabaseResourcePanel implements AzureFormJPanel<DatabaseResource> 
             // database
             this.databaseComboBox = new DatabaseComboBox<SqlServer, SqlDatabaseEntity>();
             this.databaseComboBox.setItemsLoader(() -> Objects.isNull(this.databaseComboBox.getServer()) ||
-                    !StringUtils.equals("Ready", ((MySqlServer) this.databaseComboBox.getServer()).entity().getState()) ?
+                    !StringUtils.equals("Ready", ((SqlServer) this.databaseComboBox.getServer()).entity().getState()) ?
                     Collections.emptyList() : ((SqlServer) this.databaseComboBox.getServer()).databases());
             this.databaseComboBox.setItemTextFunc((Function<SqlDatabaseEntity, String>) databaseEntity -> databaseEntity.getName());
             // username
