30acac146f3e7773a997499750f11e73fac94199
Hanxiao Liu <hanli@microsoft.com>
2021-08-02 15:04:55 +0800

    Remove outdated codes

--- PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-plugin-lib/src/main/java/com/microsoft/azure/toolkit/intellij/common/task/IntellijAzureTaskManager.java
+++ PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-plugin-lib/src/main/java/com/microsoft/azure/toolkit/intellij/common/task/IntellijAzureTaskManager.java
@@ -14,7 +14,6 @@ import com.intellij.openapi.progress.ProgressManager;
 import com.intellij.openapi.progress.Task;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Disposer;
-import com.intellij.openapi.util.registry.Registry;
 import com.microsoft.azure.toolkit.lib.common.task.AzureTask;
 import com.microsoft.azure.toolkit.lib.common.task.AzureTaskManager;
 import org.apache.commons.lang3.StringUtils;
@@ -86,11 +85,7 @@ public class IntellijAzureTaskManager extends AzureTaskManager {
         final PerformInBackgroundOption foreground = PerformInBackgroundOption.DEAF;
         // refer https://jetbrains.org/intellij/sdk/docs/basics/disposers.html
         final Disposable disposable = Disposer.newDisposable();
-        // refer https://github.com/JetBrains/intellij-community/commit/077c5558993b97cfb6f68ccc3cbe13065ba3cba8
-        // refer https://github.com/JetBrains/intellij-community/commit/d7ac4e133fec7e4c1e63f4c1d7dda65e25258b81 myBackgrounded is default to true since 2021.2
-        Registry.getAll().stream()
-                .filter(registryValue -> StringUtils.equalsIgnoreCase(registryValue.getKey(), "ide.background.tasks"))
-                .findFirst().ifPresent(registryValue -> registryValue.setValue(false, disposable));
+        // refer https://github.com/JetBrains/intellij-community/commit/d7ac4e133fec7e4c1e63f4c1d7dda65e25258b81 ide.background.tasks has been removed
         final String title = StringUtils.capitalize(task.getTitle().toString());
         final Task.Backgroundable modalTask = new Task.Backgroundable((Project) task.getProject(), title, task.isCancellable(), foreground) {
             @Override
