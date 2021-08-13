49878c1acb4155d4541f11df413ad547124c0407
andxu <andxu@microsoft.com>
2021-08-13 14:40:10 +0800

    address pr review comments

--- Utils/azuretools-core/src/com/microsoft/azuretools/sdkmanage/AzureManagerBase.java
+++ Utils/azuretools-core/src/com/microsoft/azuretools/sdkmanage/AzureManagerBase.java
@@ -272,7 +272,7 @@ public abstract class AzureManagerBase implements AzureManager {
 
     private static Authenticator createProxyAuthenticatorFromConfig() {
         final AzureConfiguration az = az().config();
-        if (StringUtils.isNotBlank(az.getProxySource()) && StringUtils.isNoneBlank(az.getProxyUsername(), az.getProxyPassword())) {
+        if (StringUtils.isNoneBlank(az.getProxySource(), az.getProxyUsername(), az.getProxyPassword())) {
             return (route, response) -> {
                 String credential = Credentials.basic(az.getProxyUsername(), az.getProxyPassword());
                 return response.request().newBuilder()
