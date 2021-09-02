69a22bc3c9e6cf2fe47e913933a8e990108e8929
Hanxiao Liu <hanli@microsoft.com>
2021-09-02 22:05:17 +0800

    Add seperate module for vm

---
+++ PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-plugin-vm/src/main/java/com/microsoft/azure/toolkit/intellij/vm/IntelliJVMActionsContributor.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ */
+
+package com.microsoft.azure.toolkit.intellij.vm;
+
+import com.microsoft.azure.toolkit.ide.common.IActionsContributor;
+
+public class IntelliJVMActionsContributor implements IActionsContributor {
+    // todo: Implement action contributor for vm and migrate explorer tree from utils
+}
