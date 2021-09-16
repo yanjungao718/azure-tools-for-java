ba4008b5397478205e17167e638acd875defc455
Mingliang Wang <wangmi@microsoft.com>
2021-09-16 16:52:46 +0800

    add resource connection action to explorer node

--- PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-plugin-storage/src/main/java/com/microsoft/azure/toolkit/intellij/storage/IntellijStorageActionsContributor.java
+++ PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-plugin-storage/src/main/java/com/microsoft/azure/toolkit/intellij/storage/IntellijStorageActionsContributor.java
@@ -8,9 +8,15 @@ package com.microsoft.azure.toolkit.intellij.storage;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.microsoft.azure.toolkit.ide.common.IActionsContributor;
 import com.microsoft.azure.toolkit.ide.common.action.ResourceCommonActionsContributor;
+import com.microsoft.azure.toolkit.intellij.connector.AzureServiceResource;
+import com.microsoft.azure.toolkit.intellij.connector.ConnectorDialog;
+import com.microsoft.azure.toolkit.intellij.storage.connection.StorageAccountResource;
 import com.microsoft.azure.toolkit.intellij.storage.creation.CreateStorageAccountAction;
 import com.microsoft.azure.toolkit.lib.common.action.AzureActionManager;
+import com.microsoft.azure.toolkit.lib.common.entity.IAzureResource;
+import com.microsoft.azure.toolkit.lib.common.task.AzureTaskManager;
 import com.microsoft.azure.toolkit.lib.storage.service.AzureStorageAccount;
+import com.microsoft.azure.toolkit.lib.storage.service.StorageAccount;
 
 import java.util.function.BiConsumer;
 import java.util.function.BiPredicate;
@@ -21,5 +27,13 @@ public class IntellijStorageActionsContributor implements IActionsContributor {
         final BiPredicate<Object, AnActionEvent> condition = (r, e) -> r instanceof AzureStorageAccount;
         final BiConsumer<Object, AnActionEvent> handler = (c, e) -> CreateStorageAccountAction.createStorageAccount((e.getProject()));
         am.registerHandler(ResourceCommonActionsContributor.CREATE, condition, handler);
+
+        am.<IAzureResource<?>, AnActionEvent>registerHandler(ResourceCommonActionsContributor.CONNECT, (r, e) -> r instanceof StorageAccount, (r, e) -> {
+            AzureTaskManager.getInstance().runLater(() -> {
+                final ConnectorDialog dialog = new ConnectorDialog(e.getProject());
+                dialog.setResource(new AzureServiceResource<>(((StorageAccount) r), StorageAccountResource.DEFINITION));
+                dialog.show();
+            });
+        });
     }
 }
