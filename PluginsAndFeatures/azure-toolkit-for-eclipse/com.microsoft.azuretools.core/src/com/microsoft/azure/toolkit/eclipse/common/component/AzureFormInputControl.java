3aca80e8b70ff2a03b73ecaf77382d9a95c49f39
Mingliang Wang <wangmi@microsoft.com>
2021-10-18 15:34:34 +0800

    use DataStore to reduce duplicate code.

---
+++ PluginsAndFeatures/azure-toolkit-for-eclipse/com.microsoft.azuretools.core/src/com/microsoft/azure/toolkit/eclipse/common/component/AzureFormInputControl.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ */
+
+package com.microsoft.azure.toolkit.eclipse.common.component;
+
+import com.microsoft.azure.toolkit.lib.common.DataStore;
+import com.microsoft.azure.toolkit.lib.common.form.AzureFormInput;
+import com.microsoft.azure.toolkit.lib.common.form.Validatable;
+import org.eclipse.core.runtime.ListenerList;
+import org.eclipse.swt.widgets.Control;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
+public interface AzureFormInputControl<T> extends AzureFormInput<T>, DataStore {
+    default Control getInputControl() {
+        return (Control) this;
+    }
+
+    default void addValueChangedListener(Consumer<T> listener) {
+        final ListenerList valueChangedListeners = this.get("valueChangedListeners", new ListenerList());
+        if (Arrays.stream(valueChangedListeners.getListeners()).noneMatch(l -> l.equals(listener))) {
+            valueChangedListeners.add(listener);
+        }
+    }
+
+    default void removeValueChangedListener(Consumer<T> listener) {
+        final ListenerList valueChangedListeners = this.get("valueChangedListeners", new ListenerList());
+        valueChangedListeners.remove(listener);
+    }
+
+    default List<Consumer<T>> getValueChangedListeners() {
+        final ListenerList valueChangedListeners = this.get("valueChangedListeners", new ListenerList());
+        return Arrays.stream(valueChangedListeners.getListeners()).map(l -> (Consumer<T>) l).collect(Collectors.toList());
+    }
+
+    default void fireValueChangedEvent(T val) {
+        this.getValueChangedListeners().forEach(l -> l.accept(val));
+    }
+
+    @Nullable
+    @Override
+    default Validatable.Validator getValidator() {
+        return this.get("validator");
+    }
+
+    default void setValidator(Validator validator) {
+        this.set("validator", validator);
+    }
+
+    @Override
+    default boolean isRequired() {
+        return this.get("required", false);
+    }
+
+    default void setRequired(boolean required) {
+        this.set("required", required);
+    }
+}
