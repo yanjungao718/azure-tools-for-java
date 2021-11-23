4e52d7281c0be16288ac73cbe1b7e4c347ca5df8
Mingliang Wang <wangmi@microsoft.com>
2021-11-23 10:24:34 +0800

    #1891898: [Test] Pops up com.azure.core.management.exception.ManagementException while deploying spring cloud with a creating service

--- PluginsAndFeatures/azure-toolkit-for-intellij/src/main/java/com/microsoft/azure/toolkit/intellij/appservice/AppServiceMonitorPanel.java
+++ PluginsAndFeatures/azure-toolkit-for-intellij/src/main/java/com/microsoft/azure/toolkit/intellij/appservice/AppServiceMonitorPanel.java
@@ -153,7 +153,6 @@ public class AppServiceMonitorPanel extends JPanel implements AzureFormPanel<Mon
         txtRetention = new AzureIntegerInput();
         txtRetention.setMinValue(0);
         txtRetention.setMaxValue(99999);
-        txtRetention.setDefaultValue(0);
         txtRetention.setRequired(false);
     }
 
