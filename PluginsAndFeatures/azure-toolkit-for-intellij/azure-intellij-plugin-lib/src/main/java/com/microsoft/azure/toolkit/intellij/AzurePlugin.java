7ed9e2fba27e8aed0af8c2d1503413fe31fd26df
Mingliang Wang <wangmi@microsoft.com>
2021-12-10 14:01:01 +0800

    refactor to simplify SignInWindow and add Help for signing in.

---
+++ PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-plugin-lib/src/main/java/com/microsoft/azure/toolkit/intellij/AzurePlugin.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ */
+
+package com.microsoft.azure.toolkit.intellij;
+
+public class AzurePlugin {
+    public static final String PLUGIN_ID = "com.microsoft.tooling.msservices.intellij.azure";
+    public static final String PLUGIN_NAME = "azure-toolkit-for-intellij";
+}
