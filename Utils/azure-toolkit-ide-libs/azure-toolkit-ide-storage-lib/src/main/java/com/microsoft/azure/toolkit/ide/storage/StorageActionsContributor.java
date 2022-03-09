7df5ae5bebcbbfee4ac874e3ff13abe584af17d7
Hanxiao Liu <hanli@microsoft.com>
2022-03-09 21:26:50 +0800

    Remove deprecated oepn storage explorer action

--- Utils/azure-toolkit-ide-libs/azure-toolkit-ide-storage-lib/src/main/java/com/microsoft/azure/toolkit/ide/storage/StorageActionsContributor.java
+++ Utils/azure-toolkit-ide-libs/azure-toolkit-ide-storage-lib/src/main/java/com/microsoft/azure/toolkit/ide/storage/StorageActionsContributor.java
@@ -8,7 +8,6 @@ package com.microsoft.azure.toolkit.ide.storage;
 import com.microsoft.azure.toolkit.ide.common.IActionsContributor;
 import com.microsoft.azure.toolkit.ide.common.action.ResourceCommonActionsContributor;
 import com.microsoft.azure.toolkit.ide.storage.action.OpenAzureStorageExplorerAction;
-import com.microsoft.azure.toolkit.ide.storage.action.OpenStorageExplorerAction;
 import com.microsoft.azure.toolkit.lib.common.action.Action;
 import com.microsoft.azure.toolkit.lib.common.action.ActionGroup;
 import com.microsoft.azure.toolkit.lib.common.action.ActionView;
@@ -26,19 +25,10 @@ public class StorageActionsContributor implements IActionsContributor {
     public static final String SERVICE_ACTIONS = "actions.storage.service";
     public static final String ACCOUNT_ACTIONS = "actions.storage.account";
 
-    public static final Action.Id<StorageAccount> OPEN_STORAGE_EXPLORER = Action.Id.of("action.storage.open_storage_explorer");
     public static final Action.Id<StorageAccount> OPEN_AZURE_STORAGE_EXPLORER = Action.Id.of("action.storage.open_azure_storage_explorer");
 
     @Override
     public void registerActions(AzureActionManager am) {
-        final Consumer<StorageAccount> open = OpenStorageExplorerAction::openStorageBrowser;
-        final ActionView.Builder openView = new ActionView.Builder("Open Storage Explorer", "/icons/action/portal.svg")
-                .title(s -> Optional.ofNullable(s).map(r -> title("storage.open_storage_explorer.account", ((StorageAccount) r).name())).orElse(null))
-                .enabled(s -> s instanceof StorageAccount);
-        final Action<StorageAccount> action = new Action<>(open, openView);
-        action.setShortcuts(am.getIDEDefaultShortcuts().view());
-        am.registerAction(OPEN_STORAGE_EXPLORER, action);
-
         final Consumer<StorageAccount> openAzureStorageExplorer = resource -> new OpenAzureStorageExplorerAction().openResource(resource);
         final ActionView.Builder openAzureStorageExplorerView = new ActionView.Builder("Open Azure Storage Explorer")
                 .title(s -> Optional.ofNullable(s).map(r -> title("storage.open_azure_storage_explorer.account", ((StorageAccount) r).name())).orElse(null))
