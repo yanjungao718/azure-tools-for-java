98990cff355cb531bcc91afeebf65c53e96846bf
Hanxiao Liu <hanli@microsoft.com>
2022-03-15 15:59:07 +0800

    Add intellij-plugin-containerregistry for acr support

---
+++ PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-plugin-containerregistry/src/main/java/com/microsoft/azure/toolkit/intellij/containerregistry/IntelliJContainerRegistryActionsContributor.java
@@ -0,0 +1,32 @@
+package com.microsoft.azure.toolkit.intellij.containerregistry;
+
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.microsoft.azure.toolkit.ide.common.IActionsContributor;
+import com.microsoft.azure.toolkit.ide.common.action.ResourceCommonActionsContributor;
+import com.microsoft.azure.toolkit.ide.containerregistry.ContainerRegistryActionsContributor;
+import com.microsoft.azure.toolkit.lib.common.action.AzureActionManager;
+import com.microsoft.azure.toolkit.lib.common.model.AzResourceBase;
+import com.microsoft.azure.toolkit.lib.containerregistry.ContainerRegistry;
+
+import java.util.function.BiConsumer;
+import java.util.function.BiPredicate;
+
+public class IntelliJContainerRegistryActionsContributor implements IActionsContributor {
+    @Override
+    public void registerHandlers(AzureActionManager am) {
+        final BiPredicate<ContainerRegistry, AnActionEvent> condition = (r, e) -> r instanceof ContainerRegistry;
+        final BiConsumer<ContainerRegistry, AnActionEvent> handler = (c, e) -> {
+        };
+        am.registerHandler(ContainerRegistryActionsContributor.PUSH_IMAGE, condition, handler);
+
+        final BiPredicate<AzResourceBase, AnActionEvent> showPropertyCondition = (r, e) -> r instanceof ContainerRegistry;
+        final BiConsumer<AzResourceBase, AnActionEvent> showPropertyHandler = (c, e) -> {
+        };
+        am.registerHandler(ResourceCommonActionsContributor.SHOW_PROPERTIES, showPropertyCondition, showPropertyHandler);
+    }
+
+    @Override
+    public int getOrder() {
+        return ContainerRegistryActionsContributor.INITIALIZE_ORDER + 1;
+    }
+}
