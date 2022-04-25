1d496dac99d1209cd7d443a8b548c248853a519d
Mingliang Wang <wangmi@microsoft.com>
2022-04-25 14:53:16 +0800

    fix compilation error in 2022.1

--- Utils/azuretools-core/src/com/microsoft/azuretools/authmanage/AuthMethodManager.java
+++ Utils/azuretools-core/src/com/microsoft/azuretools/authmanage/AuthMethodManager.java
@@ -39,6 +39,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
