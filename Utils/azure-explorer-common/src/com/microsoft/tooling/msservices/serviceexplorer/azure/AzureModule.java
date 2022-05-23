de4391c82b2fc40ae19dfb49d374bd8a6baf9195
Hanxiao Liu <hanli@microsoft.com>
2022-05-23 14:22:42 +0800

    Fix azure explorer always show sign in after restart, AB#1951390

--- Utils/azure-explorer-common/src/com/microsoft/tooling/msservices/serviceexplorer/azure/AzureModule.java
+++ Utils/azure-explorer-common/src/com/microsoft/tooling/msservices/serviceexplorer/azure/AzureModule.java
@@ -74,6 +74,7 @@ public class AzureModule extends AzureRefreshableNode {
         this.accountListener = new AzureEventBus.EventListener(e -> handleSubscriptionChange());
         AzureEventBus.on("account.subscription_changed.account", accountListener);
         AzureEventBus.on("account.logout.account", accountListener);
+        AzureEventBus.on("account.restore_sign_in", accountListener);
         handleSubscriptionChange();
     }
 
