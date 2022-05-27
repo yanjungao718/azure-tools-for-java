63da75d0b3cc8272b37578acfc6f6d798522268b
Mingliang Wang <wangmi@microsoft.com>
2022-05-27 10:59:23 +0800

    open other links in local browser.

--- PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-plugin-lib/src/main/java/com/microsoft/azure/toolkit/intellij/common/feedback/ProvideFeedbackEditor.java
+++ PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-plugin-lib/src/main/java/com/microsoft/azure/toolkit/intellij/common/feedback/ProvideFeedbackEditor.java
@@ -5,6 +5,7 @@
 
 package com.microsoft.azure.toolkit.intellij.common.feedback;
 
+import com.intellij.ide.BrowserUtil;
 import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
@@ -12,6 +13,7 @@ import com.intellij.openapi.util.Disposer;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.ui.jcef.JBCefBrowser;
 import com.intellij.ui.jcef.JBCefBrowserBase;
+import com.intellij.ui.jcef.JBCefClient;
 import com.intellij.ui.jcef.JBCefJSQuery;
 import com.intellij.uiDesigner.core.GridConstraints;
 import com.microsoft.azure.toolkit.intellij.common.BaseEditor;
@@ -19,6 +21,8 @@ import com.microsoft.azure.toolkit.lib.common.task.AzureTaskManager;
 import org.cef.browser.CefBrowser;
 import org.cef.browser.CefFrame;
 import org.cef.handler.CefLoadHandlerAdapter;
+import org.cef.handler.CefRequestHandlerAdapter;
+import org.cef.network.CefRequest;
 import org.jetbrains.annotations.Nls;
 
 import javax.annotation.Nonnull;
@@ -32,6 +36,7 @@ public class ProvideFeedbackEditor extends BaseEditor implements DumbAware {
         super(virtualFile);
         final JBCefBrowser jbCefBrowser = new JBCefBrowser("https://www.surveymonkey.com/r/PNB5NBL?mode=simple");
         final CefBrowser browser = jbCefBrowser.getCefBrowser();
+        final JBCefClient client = jbCefBrowser.getJBCefClient();
         pnlRoot.add(jbCefBrowser.getComponent(), new GridConstraints(0, 0, 1, 1, 0, GridConstraints.FILL_BOTH, 3, 3, null, null, null, 0));
         // Create a JS query instance
         this.myJSQueryOpenInBrowser = JBCefJSQuery.create((JBCefBrowserBase) jbCefBrowser);
@@ -42,11 +47,16 @@ public class ProvideFeedbackEditor extends BaseEditor implements DumbAware {
             });
             return new JBCefJSQuery.Response("ok!");
         });
+        client.addRequestHandler(openLinkWithLocalBrowser(), browser);
+        client.addLoadHandler(modifySubmitButtonInSurveyCollectionPage(), browser);
+    }
 
-        jbCefBrowser.getJBCefClient().addLoadHandler(new CefLoadHandlerAdapter() {
+    @Nonnull
+    private CefLoadHandlerAdapter modifySubmitButtonInSurveyCollectionPage() {
+        return new CefLoadHandlerAdapter() {
             @Override
             public void onLoadEnd(CefBrowser browser, CefFrame frame, int httpStatusCode) {
-                if (!browser.getURL().contains("https://www.surveymonkey.com/r/PNB5NBL")) {
+                if (!browser.getURL().contains("www.surveymonkey.com/r/PNB5NBL")) {
                     return;
                 }
                 final String js = "window.JavaPanelBridge = {\n" +
@@ -63,7 +73,23 @@ public class ProvideFeedbackEditor extends BaseEditor implements DumbAware {
                     "}";
                 browser.executeJavaScript(js, browser.getURL(), 0);
             }
-        }, browser);
+        };
+    }
+
+    @Nonnull
+    private CefRequestHandlerAdapter openLinkWithLocalBrowser() {
+        return new CefRequestHandlerAdapter() {
+            @Override
+            public boolean onBeforeBrowse(CefBrowser browser, CefFrame frame, CefRequest request, boolean user_gesture, boolean is_redirect) {
+                if (request.getURL().contains("survey-thanks")) {
+                    return true;
+                } else if (!request.getURL().contains("www.surveymonkey.com/r/PNB5NBL")) {
+                    BrowserUtil.browse(request.getURL());
+                    return true;
+                }
+                return false;
+            }
+        };
     }
 
     @Override
@@ -72,7 +98,8 @@ public class ProvideFeedbackEditor extends BaseEditor implements DumbAware {
     }
 
     @Override
-    public @Nls(capitalization = Nls.Capitalization.Title) @Nonnull String getName() {
+    public @Nls(capitalization = Nls.Capitalization.Title)
+    @Nonnull String getName() {
         return "Provide Feedback";
     }
 
