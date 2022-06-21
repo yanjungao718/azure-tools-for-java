c4cc123cfdf44df03a3b8cee05850f4303d4c0a5
Hanxiao Liu <hanli@microsoft.com>
2022-06-21 22:54:38 +0800

    Refactor git clone task to support customized parameter

--- PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-plugin-guidance/src/main/java/com/microsoft/azure/toolkit/ide/guidance/task/GitCloneTask.java
+++ PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-plugin-guidance/src/main/java/com/microsoft/azure/toolkit/ide/guidance/task/GitCloneTask.java
@@ -8,8 +8,10 @@ import com.microsoft.azure.toolkit.ide.guidance.GuidanceConfigManager;
 import com.microsoft.azure.toolkit.ide.guidance.GuidanceTask;
 import com.microsoft.azure.toolkit.lib.common.exception.AzureToolkitRuntimeException;
 import com.microsoft.azure.toolkit.lib.common.messager.AzureMessager;
+import com.microsoft.azure.toolkit.lib.common.utils.Utils;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang.StringUtils;
+import org.eclipse.jgit.api.CloneCommand;
 import org.eclipse.jgit.api.Git;
 
 import javax.annotation.Nonnull;
@@ -23,6 +25,9 @@ import static com.microsoft.azure.toolkit.ide.guidance.GuidanceConfigManager.GET
 public class GitCloneTask implements GuidanceTask {
     public static final String DIRECTORY = "directory";
     public static final String DEFAULT_GIT_DIRECTORY = "defaultGitDirectory";
+    public static final String BRANCH = "branch";
+    public static final String REPOSITORY = "repository";
+    public static final String REPOSITORY_PATH = "repository_path";
     private final Guidance guidance;
     private final ComponentContext context;
 
@@ -47,29 +52,34 @@ public class GitCloneTask implements GuidanceTask {
 
     @Override
     public void init() {
-        final String defaultPath = new File(System.getProperty("user.home"), context.getGuidance().getName()).getAbsolutePath();
+        final String directoryName = String.format("%s-%s", context.getGuidance().getName(), Utils.getTimestamp());
+        final String defaultPath = new File(System.getProperty("user.home"), directoryName).getAbsolutePath();
         this.context.applyResult(DEFAULT_GIT_DIRECTORY, defaultPath);
     }
 
     @Override
     public void execute() throws Exception {
-        final String projectPath = (String) context.getParameter(DIRECTORY);
-        final String gitUrl = "https://github.com/spring-guides/gs-spring-boot.git";
+        final String repository = (String) context.getParameter(REPOSITORY);
+        final String branch = (String) context.getParameter(BRANCH);
+        final String repositoryPath = (String) context.getParameter(REPOSITORY_PATH);
+        final String directory = (String) context.getParameter(DIRECTORY);
         try {
-            Git.cloneRepository()
-                    .setURI(gitUrl)
-                    .setDirectory(Paths.get(projectPath).toFile())
-                    .call();
+            final CloneCommand cloneCommand = Git.cloneRepository().setURI(repository).setDirectory(Paths.get(directory).toFile());
+            if (StringUtils.isNotBlank(branch)) {
+                cloneCommand.setBranch(branch);
+            }
+            cloneCommand.call();
             // Copy get start file to path
-            copyConfigurationToWorkspace(projectPath);
-            ProjectUtil.openOrImport(Paths.get(projectPath, "complete"), OpenProjectTask.newProject());
+            final File target = StringUtils.isEmpty(repositoryPath) ? new File(directory) : new File(directory, repositoryPath);
+            copyConfigurationToWorkspace(target);
+            ProjectUtil.openOrImport(target.toPath(), OpenProjectTask.newProject());
         } catch (final Exception ex) {
             AzureMessager.getMessager().error(ex);
             throw new AzureToolkitRuntimeException(ex);
         }
     }
 
-    private void copyConfigurationToWorkspace(final String projectPath) throws IOException {
+    private void copyConfigurationToWorkspace(final File target) throws IOException {
         if (StringUtils.isEmpty(guidance.getUri())) {
             return;
         }
@@ -77,8 +87,7 @@ public class GitCloneTask implements GuidanceTask {
             if (inputStream == null) {
                 return;
             }
-            final File complete = new File(projectPath, "complete");
-            FileUtils.copyInputStreamToFile(inputStream, new File(complete, GuidanceConfigManager.GETTING_START_CONFIGURATION_NAME));
+            FileUtils.copyInputStreamToFile(inputStream, new File(target, GuidanceConfigManager.GETTING_START_CONFIGURATION_NAME));
         }
     }
 }
