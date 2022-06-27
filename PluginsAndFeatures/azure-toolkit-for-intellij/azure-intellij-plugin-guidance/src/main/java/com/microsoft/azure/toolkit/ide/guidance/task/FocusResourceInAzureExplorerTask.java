1219858699f573d7b66a0f30e752db887bdd2006
Mingliang Wang <wangmi@microsoft.com>
2022-06-27 09:56:27 +0800

    implement task guidance task `task.common.focus_resource_in_explorer` based on action `HIGHLIGHT_RESOURCE_IN_EXPLORER`

---
+++ PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-plugin-guidance/src/main/java/com/microsoft/azure/toolkit/ide/guidance/task/FocusResourceInAzureExplorerTask.java
@@ -0,0 +1,39 @@
+package com.microsoft.azure.toolkit.ide.guidance.task;
+
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.actionSystem.DataContext;
+import com.intellij.openapi.actionSystem.EmptyAction;
+import com.microsoft.azure.toolkit.ide.common.action.ResourceCommonActionsContributor;
+import com.microsoft.azure.toolkit.ide.guidance.ComponentContext;
+import com.microsoft.azure.toolkit.ide.guidance.GuidanceTask;
+import com.microsoft.azure.toolkit.lib.Azure;
+import com.microsoft.azure.toolkit.lib.common.action.AzureActionManager;
+import com.microsoft.azure.toolkit.lib.common.model.AbstractAzResource;
+import lombok.RequiredArgsConstructor;
+
+import javax.annotation.Nonnull;
+
+@RequiredArgsConstructor
+public class FocusResourceInAzureExplorerTask implements GuidanceTask {
+    public static final String RESOURCE_ID = "resourceId";
+    public static final String ID = "task.common.focus_resource_in_explorer";
+
+    @Nonnull
+    private final ComponentContext context;
+
+    @Override
+    public void execute() {
+        final String resourceId = (String) context.getParameter(RESOURCE_ID);
+        final AbstractAzResource<?, ?, ?> resource = Azure.az().getById(resourceId);
+        final DataContext context = dataId -> CommonDataKeys.PROJECT.getName().equals(dataId) ? this.context.getProject() : null;
+        final AnActionEvent event = AnActionEvent.createFromAnAction(new EmptyAction(), null, "azure.guidance.summary", context);
+        AzureActionManager.getInstance().getAction(ResourceCommonActionsContributor.HIGHLIGHT_RESOURCE_IN_EXPLORER).handle(resource, event);
+    }
+
+    @Nonnull
+    @Override
+    public String getName() {
+        return ID;
+    }
+}
