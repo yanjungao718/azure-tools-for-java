0d8a55a9ecbe63bcfc6aba0c027030474bfc2692
Mingliang Wang <wangmi@microsoft.com>
2022-07-14 00:23:12 +0800

    implement BrowsableTargetEnvironmentType to enable browsing remote files.

--- PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-plugin-vm/src/main/java/com/microsoft/azure/toolkit/intellij/vm/runtarget/AzureVmTargetType.java
+++ PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-plugin-vm/src/main/java/com/microsoft/azure/toolkit/intellij/vm/runtarget/AzureVmTargetType.java
@@ -1,8 +1,10 @@
 package com.microsoft.azure.toolkit.intellij.vm.runtarget;
 
 import com.intellij.execution.Platform;
+import com.intellij.execution.target.BrowsableTargetEnvironmentType;
 import com.intellij.execution.target.CustomToolLanguageRuntimeType;
 import com.intellij.execution.target.LanguageRuntimeType;
+import com.intellij.execution.target.TargetEnvironmentConfiguration;
 import com.intellij.execution.target.TargetEnvironmentRequest;
 import com.intellij.execution.target.TargetEnvironmentType;
 import com.intellij.execution.target.TargetPlatform;
@@ -11,8 +13,18 @@ import com.intellij.ide.wizard.AbstractWizardStepEx;
 import com.intellij.openapi.components.PersistentStateComponent;
 import com.intellij.openapi.options.Configurable;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.ui.TextComponentAccessor;
+import com.intellij.openapi.util.NlsContexts;
+import com.intellij.remote.AuthType;
+import com.intellij.ssh.config.unified.SshConfig;
+import com.intellij.ssh.ui.unified.SshUiData;
+import com.jetbrains.plugins.remotesdk.RemoteSdkBundle;
 import com.jetbrains.plugins.remotesdk.target.ssh.target.SshRemoteEnvironmentRequest;
 import com.jetbrains.plugins.remotesdk.target.ssh.target.SshTargetConfigurable;
+import com.jetbrains.plugins.remotesdk.target.ssh.target.SshTargetEnvironmentConfiguration;
+import com.jetbrains.plugins.remotesdk.target.ssh.target.SshTargetType;
+import com.jetbrains.plugins.remotesdk.target.ssh.target.TempSshTargetEnvironmentConfigurationBase;
 import com.jetbrains.plugins.remotesdk.target.ssh.target.wizard.ConnectionData;
 import com.jetbrains.plugins.remotesdk.target.ssh.target.wizard.SshConfigureCustomToolStep;
 import com.jetbrains.plugins.remotesdk.target.ssh.target.wizard.SshTargetAuthStep;
@@ -27,16 +39,25 @@ import com.microsoft.azure.toolkit.intellij.vm.runtarget.wizard.AzureVmTarget3In
 import com.microsoft.azure.toolkit.intellij.vm.runtarget.wizard.AzureVmTarget4ConfigureCustomToolStep;
 import com.microsoft.azure.toolkit.intellij.vm.runtarget.wizard.AzureVmTarget5LanguageStep;
 import kotlin.collections.CollectionsKt;
+import kotlin.jvm.internal.Intrinsics;
 import lombok.Getter;
+import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.Nls;
+import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
 import java.util.List;
+import java.util.Optional;
+import java.util.function.Supplier;
 
 @Getter
-public class AzureVmTargetType extends TargetEnvironmentType<AzureVmTargetEnvironmentConfiguration> {
+public class AzureVmTargetType extends TargetEnvironmentType<AzureVmTargetEnvironmentConfiguration> implements BrowsableTargetEnvironmentType {
     public static final String TYPE_ID = "Microsoft.Compute/virtualMachines";
     public static final String DISPLAY_NAME = "Azure Virtual Machine";
     @Nls
@@ -44,26 +65,11 @@ public class AzureVmTargetType extends TargetEnvironmentType<AzureVmTargetEnviro
     private final String displayName = DISPLAY_NAME;
     @Nonnull
     private final Icon icon = IntelliJAzureIcons.getIcon(AzureIcons.VirtualMachine.MODULE);
+    private final SshTargetType originType;
 
     public AzureVmTargetType() {
         super(TYPE_ID);
-    }
-
-    @Nonnull
-    @Override
-    public PersistentStateComponent<?> createSerializer(@Nonnull AzureVmTargetEnvironmentConfiguration config) {
-        return config;
-    }
-
-    @Nonnull
-    @Override
-    public AzureVmTargetEnvironmentConfiguration createDefaultConfig() {
-        return new AzureVmTargetEnvironmentConfiguration();
-    }
-
-    @Override
-    public boolean providesNewWizard(@Nonnull Project project, @Nullable LanguageRuntimeType<?> runtimeType) {
-        return true;
+        this.originType = new SshTargetType();
     }
 
     @Nullable
@@ -86,6 +92,24 @@ public class AzureVmTargetType extends TargetEnvironmentType<AzureVmTargetEnviro
 
     @Nonnull
     @Override
+    public PersistentStateComponent<?> createSerializer(@Nonnull AzureVmTargetEnvironmentConfiguration config) {
+        return config;
+    }
+
+    @Nonnull
+    @Override
+    public AzureVmTargetEnvironmentConfiguration createDefaultConfig() {
+        return new AzureVmTargetEnvironmentConfiguration();
+    }
+
+    @Override
+    public boolean providesNewWizard(@Nonnull Project project, @Nullable LanguageRuntimeType<?> runtimeType) {
+        return true;
+    }
+
+
+    @Nonnull
+    @Override
     public AzureVmTargetEnvironmentConfiguration duplicateConfig(@Nonnull AzureVmTargetEnvironmentConfiguration config) {
         return duplicateTargetConfiguration(this, config);
     }
@@ -101,5 +125,28 @@ public class AzureVmTargetType extends TargetEnvironmentType<AzureVmTargetEnviro
     @SuppressWarnings("UnstableApiUsage")
     public TargetEnvironmentRequest createEnvironmentRequest(@Nonnull Project project, @Nonnull AzureVmTargetEnvironmentConfiguration config) {
         return new SshRemoteEnvironmentRequest(project, config, new TargetPlatform(Platform.UNIX));
+    }
+
+    @Override
+    public @NotNull <T extends Component> ActionListener createBrowser(@NotNull Project project, @NlsContexts.DialogTitle String title, @NotNull TextComponentAccessor<T> textComponentAccessor, @NotNull T component, @NotNull Supplier<TargetEnvironmentConfiguration> configurationSupplier) {
+        return new ActionListener() {
+            public final void actionPerformed(ActionEvent it) {
+                final TargetEnvironmentConfiguration configuration = configurationSupplier.get();
+                if (configuration instanceof SshTargetEnvironmentConfiguration) {
+                    final SshConfig sshConfig = ((SshTargetEnvironmentConfiguration) configuration).findSshConfig(project);
+                    final SshUiData uiData = sshConfig != null ? new SshUiData(sshConfig, true) : null;
+                    SshTargetType.Companion.handleBrowsing$intellij_remoteRun(uiData, project, title, component, textComponentAccessor);
+                } else if (configuration instanceof TempSshTargetEnvironmentConfigurationBase) {
+                    final SshUiData uiData = ((TempSshTargetEnvironmentConfigurationBase) configuration).getTempSshData();
+                    final SshConfig config = uiData.getConfig();
+                    if (config.getAuthType() == AuthType.KEY_PAIR && StringUtils.isBlank(config.getKeyPath())) {
+                        config.setKeyPath(System.getProperty("user.home") + File.separator + ".ssh" + File.separator + "id_rsa");
+                    }
+                    SshTargetType.Companion.handleBrowsing$intellij_remoteRun(uiData, project, title, component, textComponentAccessor);
+                } else {
+                    Messages.showWarningDialog(component, RemoteSdkBundle.message("dialog.message.got.unexpected.settings.for.browsing", new Object[0]), title);
+                }
+            }
+        };
     }
 }
