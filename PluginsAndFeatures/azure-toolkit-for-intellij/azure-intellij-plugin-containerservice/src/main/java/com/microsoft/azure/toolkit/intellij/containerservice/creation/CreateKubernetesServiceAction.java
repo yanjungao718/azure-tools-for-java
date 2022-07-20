8cb0590eeb2b46acef3e6e5ed2846e585c284509
Hanxiao Liu <hanli@microsoft.com>
2022-07-20 23:15:28 +0800

    Create kubernetes cluster in new resource group by default

--- PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-plugin-containerservice/src/main/java/com/microsoft/azure/toolkit/intellij/containerservice/creation/CreateKubernetesServiceAction.java
+++ PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-plugin-containerservice/src/main/java/com/microsoft/azure/toolkit/intellij/containerservice/creation/CreateKubernetesServiceAction.java
@@ -5,10 +5,12 @@
 
 package com.microsoft.azure.toolkit.intellij.containerservice.creation;
 
+import com.google.common.base.Preconditions;
 import com.intellij.openapi.project.Project;
 import com.microsoft.azure.toolkit.lib.Azure;
 import com.microsoft.azure.toolkit.lib.auth.AzureAccount;
 import com.microsoft.azure.toolkit.lib.common.bundle.AzureString;
+import com.microsoft.azure.toolkit.lib.common.model.Subscription;
 import com.microsoft.azure.toolkit.lib.common.operation.AzureOperation;
 import com.microsoft.azure.toolkit.lib.common.operation.OperationBundle;
 import com.microsoft.azure.toolkit.lib.common.task.AzureTaskManager;
@@ -19,10 +21,14 @@ import com.microsoft.azure.toolkit.lib.containerservice.KubernetesClusterModule;
 import com.microsoft.azure.toolkit.lib.resource.AzureResources;
 import com.microsoft.azure.toolkit.lib.resource.ResourceGroup;
 import com.microsoft.azure.toolkit.lib.resource.task.CreateResourceGroupTask;
+import org.apache.commons.collections4.CollectionUtils;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.util.List;
 import java.util.Objects;
+
+import static com.microsoft.azure.toolkit.lib.Azure.az;
 
 public class CreateKubernetesServiceAction {
     public static void create(@Nonnull Project project, @Nullable KubernetesClusterDraft.Config data) {
@@ -40,11 +46,18 @@ public class CreateKubernetesServiceAction {
         });
     }
 
-    public static KubernetesClusterDraft.Config getDefaultConfig(final ResourceGroup resourceGroup){
-        final KubernetesClusterDraft.Config config = new KubernetesClusterDraft.Config();
+    public static KubernetesClusterDraft.Config getDefaultConfig(final ResourceGroup resourceGroup) {
+        final List<Subscription> selectedSubscriptions = az(AzureAccount.class).account().getSelectedSubscriptions();
+        Preconditions.checkArgument(CollectionUtils.isNotEmpty(selectedSubscriptions), "There are no subscriptions in your account.");
         final String name = String.format("kubernetes-service-%s", Utils.getTimestamp());
+        final String defaultResourceGroupName = String.format("rg-%s", name);
+        final Subscription subscription = resourceGroup == null ? selectedSubscriptions.get(0) : resourceGroup.getSubscription();
+        final ResourceGroup group = resourceGroup == null ? az(AzureResources.class).groups(subscription.getId())
+                .create(defaultResourceGroupName, defaultResourceGroupName) : resourceGroup;
+        final KubernetesClusterDraft.Config config = new KubernetesClusterDraft.Config();
         config.setName(name);
-        config.setResourceGroup(resourceGroup);
+        config.setSubscription(subscription);
+        config.setResourceGroup(group);
         config.setDnsPrefix(String.format("%s-dns", name));
         config.setMinVMCount(3);
         config.setMaxVMCount(5);
