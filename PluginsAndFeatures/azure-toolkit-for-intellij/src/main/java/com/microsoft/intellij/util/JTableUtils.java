751599878d9945658719f2c9c7a6860d97887cf1
Mingliang Wang <wangmi@microsoft.com>
2022-07-22 15:31:39 +0800

    enhance user experience of selecting subscriptions

--- PluginsAndFeatures/azure-toolkit-for-intellij/src/main/java/com/microsoft/intellij/util/JTableUtils.java
+++ PluginsAndFeatures/azure-toolkit-for-intellij/src/main/java/com/microsoft/intellij/util/JTableUtils.java
@@ -6,12 +6,15 @@
 package com.microsoft.intellij.util;
 
 import com.intellij.profile.codeInspection.ui.table.ThreeStateCheckBoxRenderer;
+import com.microsoft.azure.toolkit.lib.common.utils.TailingDebouncer;
 
 import javax.swing.*;
 import javax.swing.event.TableModelEvent;
 import javax.swing.event.TableModelListener;
-import javax.swing.table.*;
-import java.awt.event.*;
+import javax.swing.table.TableColumn;
+import javax.swing.table.TableModel;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.Objects;
 
 public class JTableUtils {
@@ -30,6 +33,7 @@ public class JTableUtils {
     private static class CheckboxHeaderMouseListener extends MouseAdapter implements TableModelListener {
         private final JTable table;
         private final int colIndex;
+        private final TailingDebouncer updateHeaderValue;
         private boolean updatingHeaderValue = false;
 
         public CheckboxHeaderMouseListener(final JTable table, final int columnIndex) {
@@ -37,6 +41,7 @@ public class JTableUtils {
             this.table = table;
             this.colIndex = columnIndex;
             this.table.getModel().addTableModelListener(this);
+            this.updateHeaderValue = new TailingDebouncer(() -> this.updateHeaderValueInner(), 300);
         }
 
         /**
@@ -50,8 +55,13 @@ public class JTableUtils {
                 final Object value = checkboxColumn.getHeaderValue();
                 final boolean checked = Objects.nonNull(value) && (boolean) value;
                 checkboxColumn.setHeaderValue(!checked);
-                this.updateTableModel(!checked);
                 this.table.getTableHeader().repaint();
+                this.updatingHeaderValue = true;
+                final int rowCount = table.getModel().getRowCount();
+                for (int rowIndex = 0; rowIndex < rowCount; rowIndex++) {
+                    table.getModel().setValueAt(!checked, rowIndex, this.colIndex);
+                }
+                this.updatingHeaderValue = false;
             }
         }
 
@@ -61,30 +71,21 @@ public class JTableUtils {
         @Override
         public void tableChanged(final TableModelEvent e) {
             if ((e.getColumn() == TableModelEvent.ALL_COLUMNS || e.getColumn() == this.colIndex)
-                    && !this.updatingHeaderValue) {
-                final TableModel model = this.table.getModel();
-                this.updateHeaderValue(model);
+                && !this.updatingHeaderValue) {
+                this.updateHeaderValue.debounce();
             }
         }
 
-        private void updateTableModel(final boolean headerValue) {
-            final int rowCount = table.getModel().getRowCount();
-            this.updatingHeaderValue = true;
-            for (int rowIndex = 0; rowIndex < rowCount; rowIndex++) {
-                table.getModel().setValueAt(headerValue, rowIndex, this.colIndex);
-            }
-            this.updatingHeaderValue = false;
-        }
-
-        private void updateHeaderValue(final TableModel model) {
+        private void updateHeaderValueInner() {
             int numSelected = 0;
+            final TableModel model = this.table.getModel();
             for (int row = 0; row < model.getRowCount(); row++) {
                 if ((Boolean) model.getValueAt(row, this.colIndex)) {
                     numSelected++;
                 }
             }
             final TableColumn column = table.getColumnModel().getColumn(this.colIndex);
-            if (numSelected == this.table.getModel().getRowCount()) {
+            if (numSelected == model.getRowCount()) {
                 column.setHeaderValue(true);
             } else if (numSelected == 0) {
                 column.setHeaderValue(false);
